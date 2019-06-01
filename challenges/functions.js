4958e28d04bf057f5968d3ae0db001d002112005
Classic-username <josh.drapergame@gmail.com>
2019-05-31 20:13:02 -0600

    completed with functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(arg1, arg2, cb){
+  return cb(arg1, arg2);
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +17,22 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2){
+  return num1 + num2;
+};
+
+function multiply(num1, num2){
+  return num1 * num2;
+};
+
+function greeting(fName, lName){
+  return `Hello ${fName} ${lName}, nice to meet you!`;
+};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -40,3 +54,7 @@ function myFunction() {
   nestedFunction();
 }
 myFunction();
+
+// ## MY ANSWER ##
+// nestedFunction can acces internal because nested function is within the myfunction scope. If nestedFunction had a variable within it, myFunction couldn't access it because myFunction is within global scope, but not within nestedFunction scope.
+//Likewise if there was another function (anotherFunc) entirely with a variable inside, myFunction couldn't access the variable within anotherFunc, because myFunction isn't within that scope.
