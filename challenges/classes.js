9e0d1bfa947612838e3ce00fb48a7a3d62c94f75
Classic-username <josh.drapergame@gmail.com>
2019-05-31 21:46:55 -0600

    I completed all stretch goals

--- challenges/classes.js
+++ challenges/classes.js
@@ -28,6 +28,25 @@ const cuuboid = new CuuboidMaker({
 console.log(cuuboid.volume()); // 100
 console.log(cuuboid.surfaceArea()); // 130
 
+class CubeMaker extends CuuboidMaker {
+    constructor(props){
+        super(props);
+        this.lwh = props.lwh;
+    };
+
+    volCube(){
+        return Math.pow(this.lwh, 3);
+    };
+    surfCube(){
+        return 6*(Math.pow(this.lwh, 2));
+    };
+};
+
+let cube = new CubeMaker({
+    lwh: 4,
+})
+console.log(cube.volCube())
+console.log(cube.surfCube())
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
 
 //I had to change the names in this because when I tried to use the browser to debug it kept saying 'cuboid' already declared. I got the right answers both times.
